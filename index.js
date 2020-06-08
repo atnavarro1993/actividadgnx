b659d9d970f6cd9bae3dc8e49af3d44d5fc24b00
Ariel <a.t.navarro.1993@gmail.com>
2020-06-08 15:09:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const gnx = require('@simtlix/gnx');
+const app = express();
+
+
+mongoose.connect('mongodb://localhost:27017,localhost:27018,localhost:27019/example', { replicaSet: 'rs' });
+mongoose.connection.once('open',()=>{
+    console.log('connected to database');
+});
+
+/*
+const types = require('./types');
+const includedTypes = Object.values(types);
+const schema = gnx.createSchema(includedTypes,includedTypes);
+*/
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql: true,
+}))
+
+app.listen(3000,()=>{
+    console.log('listening on port 30000')
+})
